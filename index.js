2f24865bcccbc7f437f37ba01b004c6560d8bd39
Efrain Perez Jr <efrainperezjr@live.com>
2018-03-19 16:50:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,10 +11,30 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ cart.unshift({itemName: item, itemPrice: Math.floor(Math.random()*100)})
+ return `${cart[0].itemName} has been added to your cart.`
 }
 
 function viewCart() {
   // write your code here
+  message = "Your shopping cart is empty"
+  for (var i = 0; i < cart.length; i++)
+  {
+    if (i == 0)
+    {
+      message = `In your cart, you have ${cart[i].itemName} at $${cart[i].itemPrice}`
+    }
+    else if (i < cart.length-1 && length > 2)
+    {
+      message = `${message}, ${cart[i].itemName} at $${cart[i].itemPrice}`
+    }
+    else
+    {
+      message = `${message}, and ${cart[i].itemName} at $${cart[i].itemPrice}`
+    }
+  }
+  message = `${message}.`
+  return message
 }
 
 function total() {
