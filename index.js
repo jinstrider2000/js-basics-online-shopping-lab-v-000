aa72b398505b0f42a0f921e5f97655b6439974ed
Efrain Perez Jr <efrainperezjr@live.com>
2018-03-19 18:31:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -77,6 +77,7 @@ function placeOrder(cardNumber) {
     message = "Sorry, we don't have a credit card on file for you.";
   } else {
     message = `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
+    setCart([]);
   }
   console.log(message);
   return message;
